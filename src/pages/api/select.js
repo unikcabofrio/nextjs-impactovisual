c1adaf1a485a2b615aa578fd6649621a55ccc34f
unikcabofrio <contato.unikcabofrio@gmail.com>
2023-05-22 13:38:03 -0300

    '22051338'

--- src/pages/api/select.js
+++ src/pages/api/select.js
@@ -1,10 +1,28 @@
-import db from '@/pages/api/db'
+import mysql from 'mysql2'
+
+const connection = mysql.createConnection({
+  // eslint-disable-next-line no-undef
+  host: process.env.GC_MYSQLHOST,
+  // eslint-disable-next-line no-undef
+  user: process.env.GC_MYSQLUSER,
+  // eslint-disable-next-line no-undef
+  port: process.env.GC_MYSQLPORT,
+  // eslint-disable-next-line no-undef
+  password: process.env.GC_MYSQLPASSWORD,
+  // eslint-disable-next-line no-undef
+  database: process.env.GC_MYSQLDATABASE
+})
+
+connection.connect((err) => {
+  if (err) return err
+  return true
+})
 
 export default async function handler(req, res) {
   try {
     const { table } = req.body
 
-    db.query(`SELECT * FROM ${table}`, (err, results) => {
+    connection.query(`SELECT * FROM ${table}`, (err, results) => {
       if (err) return res.status(200).json(err)
       res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
       return res.status(200).json(results)
