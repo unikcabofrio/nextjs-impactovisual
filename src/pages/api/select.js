2042a7b829a4bdff58e1209e070502a5ad4f8cca
unikcabofrio <contato.unikcabofrio@gmail.com>
2023-05-22 13:55:04 -0300

    '22051355'

--- src/pages/api/select.js
+++ src/pages/api/select.js
@@ -1,32 +1,33 @@
 import mysql from 'mysql2'
 
-const connection = mysql.createConnection({
-  // eslint-disable-next-line no-undef
-  host: process.env.GC_MYSQLHOST,
-  // eslint-disable-next-line no-undef
-  user: process.env.GC_MYSQLUSER,
-  // eslint-disable-next-line no-undef
-  port: process.env.GC_MYSQLPORT,
-  // eslint-disable-next-line no-undef
-  password: process.env.GC_MYSQLPASSWORD,
-  // eslint-disable-next-line no-undef
-  database: process.env.GC_MYSQLDATABASE
-})
-
-connection.connect((err) => {
-  if (err) return err
-  return true
-})
-
 export default async function handler(req, res) {
   try {
     const { table } = req.body
 
-    connection.query(`SELECT * FROM ${table}`, (err, results) => {
-      if (err) return res.status(200).json(err)
-      res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
-      return res.status(200).json(results)
+    const connection = mysql.createConnection({
+      // eslint-disable-next-line no-undef
+      host: process.env.GC_MYSQLHOST,
+      // eslint-disable-next-line no-undef
+      user: process.env.GC_MYSQLUSER,
+      // eslint-disable-next-line no-undef
+      port: process.env.GC_MYSQLPORT,
+      // eslint-disable-next-line no-undef
+      password: process.env.GC_MYSQLPASSWORD,
+      // eslint-disable-next-line no-undef
+      database: process.env.GC_MYSQLDATABASE
     })
+
+    connection.connect()
+
+    connection.query(`SELECT * FROM ${table}`, (error, results) => {
+      if (error) {
+        return res.status(200).json(error)
+      } else {
+        return res.status(200).json(results)
+      }
+    })
+
+    connection.end()
   } catch (error) {
     return res.status(error.statusCode || 500).json(error)
   }
