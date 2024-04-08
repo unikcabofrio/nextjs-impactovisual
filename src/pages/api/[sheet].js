b7ebf48514bfedd07ef14bb66fe28d63c403f8a8
unikcabofrio <contato.unikcabofrio@gmail.com>
2024-04-07 21:38:19 -0300

    'api'

--- src/pages/api/[sheet].js
+++ src/pages/api/[sheet].js
@@ -9,7 +9,8 @@ export default async function handler(req, res) {
     try {
 
         if (req.method === 'GET') {
-
+            console.log(url+`?SheetByName=${sheet}`)
+            
             const response = await fetch(url+`?SheetByName=${sheet}`)
             const data = await response.json()
             res.status(200).json(data)
