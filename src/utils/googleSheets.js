9b4b016312484cf851d49c135a061c06e43a4dbe
unikcabofrio <contato.unikcabofrio@gmail.com>
2024-06-10 20:32:12 -0300

    'update_revalidate'

--- src/utils/googleSheets.js
+++ src/utils/googleSheets.js
@@ -45,7 +45,7 @@ async function GetDataSheet(sheet) {
         const sheet_id = process.env.SHEET_ID
         const ulr = sheet_url.replace('::ID', sheet_id)
 
-        const response = await fetch(`${ulr}?sheet=${sheet}`,{ next: { revalidate: 60000 } });
+        const response = await fetch(`${ulr}?sheet=${sheet}`,{ next: { revalidate: 3600 } });
         const values = await response.json();
         const data = []
         values.jsonData.forEach(element => {
